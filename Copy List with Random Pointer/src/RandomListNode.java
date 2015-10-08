f59699fd7d8ef4311e07717ddca5a277dbcfaeb7
andy0727 <andy-0727@hotmail.com>
2015-10-08 11:49:13 -0700

    second

--- Copy List with Random Pointer/src/RandomListNode.java
+++
@@ -1,6 +0,0 @@
-
-public class RandomListNode {
-	int label;
-	RandomListNode next, random;
-	RandomListNode(int l){this.label=l;} 
-}
--- Insert Interval/src/Interval.java
+++
@@ -1,7 +0,0 @@
-
-public class Interval {
-	int start;
-	int end;
-	public Interval(){start=0;end=0;}
-	public Interval(int s, int e){start=s;end=e;}
-}
